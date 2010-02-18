16d948223aad98326d03aa94545b76c25f62d11b
Michal Bojanowski <michal2992@gmail.com>
2010-02-18 19:33:28 +0000

    Added example dataset with documentation. Modifed the notes in 'misc'

--- misc/pkgnet.R
+++ misc/pkgnet.R
@@ -1,121 +1,25 @@
-library(utils)
-
-# package matrix
 a <- available.packages()
 
-# split on commas and remove new-lines
-alt <- strsplit( gsub("\n", "", a[,"Depends"]), ", *")
-
-# get rid of package versions and other rubbish
-el <- lapply(alt, function(x) 
-{
-    rval <- gsub( "\\(.*\\)", "", x)
-    na.omit( gsub(" +", "", rval) )
-} )
-
-ego <- names(el)
-alter <- unique(unlist(el))
-
-# compare name sets
-length(ego)
-length(alter)
-length(intersect(ego, alter))
-setdiff(alter, ego)
-length( setdiff(ego, alter) )
-
-
-library(igraph)
-len <- sapply(el, length)
-
-
-vnams <- c("Version", "Priority", "Bundle", "License", "File",
-    "Repository")
-
-nodes <- data.frame( name=sort(unique(c(ego, alter))))
-mv <- match(nodes$name, a[,"Package"])
-z <- a[mv, vnams]
-rownames(z) <- NULL
-nodes <- cbind( nodes, z )
-
-edges <- data.frame(ego=rep(names(el), len), alter=unlist(el))
-
-# pełna sieć ze wszystkimi śmieciami (R, MASS etc.)
-g <- graph.data.frame(edges, vertices=nodes)
+library(pkgnet)
+g0 <- pkgnet(a)
+g <- delete.vertices(g0, V(g0)[ name == "R" ] )
+g <- delete.vertices(g, V(g)[ Priority %in% "recommended" ] )
+bpkg <- c("stats", "tools", "utils", "methods", "stats4", "rgl", "tcltk")
+g <- delete.vertices(g, V(g)[ name %in% bpkg ] )
 summary(g)
 
-
-g1 <- delete.vertices(g, which( V(g)$name == "R") -1 )
-
-
-
-set.seed(2992)
-k <- layout.fruchterman.reingold(g1)
-save(k, file="gfull-fr.rda")
-
-plot(g1, vertex.label=NA, vertex.size=2, layout=k)
+npkg <- c("igraph", "network", "graph")
+n <- neighborhood(g, 1, V(g)[ name %in% npkg ] )
+sg <- subgraph(g, unique(unlist(n)) )
+summary(sg)
 
 
+koords <- layout.fruchterman.reingold(sg, params=list(niter=70))
+u <- sort(unique(E(sg)$type))
+typ <- match( E(sg)$type, u)
+plot(sg, vertex.size=1, vertex.label=V(sg)$name, layout=koords,
+    edge.arrow.size=.5, vertex.label.cex=.9, edge.color=typ)
+legend("topleft", legend=u, lty=1, col=seq(along=typ))
 
-
-pkgnet <- function(a, enams=c("Depends", "Suggests", "Contains", "Imports", "Enhances"),
-    vnams=c("Version", "Priority", "Bundle", "License", "File", "Repository") )
-{
-    # list of edgelists and nodes lists
-    l <- lapply(enams, function(vn)
-        {
-            # split on commas and drop newline
-            alt <- strsplit( gsub("\n", "", a[,vn]), ", *")
-            # get rid of package versions
-            el <- lapply(alt, function(x)
-                {
-                    rval <- gsub( "\\(.*\\)", "", x)
-                    na.omit( gsub(" +", "", rval) )
-                } )
-            len <- sapply(el, length)
-            # edge list
-            edges <- data.frame( ego=rep(names(el), len), alter=unlist(el), type=rep(vn, sum(len)))
-            # node list
-            nodes <- data.frame(name=sort(unique(c(names(el), unlist(el)))))
-            # add package attributes for 'a'
-            mv <- match(nodes$name, a[,"Package"])
-            z <- a[mv, vnams]
-            rownames(z) <- NULL
-            nodes <- cbind( nodes, z )
-            list(nodes=nodes, edges=edges)
-        } )
-     nodes <- do.call("rbind", lapply(l, "[[", "nodes"))
-     nodes <- unique(nodes)
-     edges <- do.call("rbind", lapply(l, "[[", "edges"))
-     rval <- igraph::graph.data.frame(edges, vertices=nodes)
-     rval
-}
-
-
-
-
-g <- pkgnet(a)
-
-
-
-# select the largest component
-sg <- delete.vertices(g, V(g)[ name == "R" ] )
-k <- clusters(sg)
-ids <- which(k$membership == 1) - 1
-sg <- subgraph(sg, ids)
-
-koords <- layout.fruchterman.reingold(sg, params=list(niter=30))
-
-typ <- E(sg)$type
-
-
-library(RColorBrewer)
-palette(brewer.pal(8, "Dark2"))
-
-pdf("netall.pdf", 70, 70)
-plot(sg, vertex.size=.3, vertex.label=NA, edge.color=match(typ, unique(typ)),
-    layout=koords, edge.arrow.size=.3, vertex.color="black")
-dev.off()
-
-
-# subgraph based on edges
-gdep <- 
+crannet <- g0
+save(crannet, file="crannet.rda")
